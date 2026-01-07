be3c0916d2ff9ccbec4f99393030730e12c30d27
Mat Jordan <mat@northwestern.edu>
2026-01-07 13:35:53 -0500

    Update Canopy, remove get-started, add Biblipography.

--- app/components/mdx.tsx
+++ app/components/mdx.tsx
@@ -12,4 +12,5 @@ export const components = {
 // them separately and hydrates placeholders at runtime.
 export const clientComponents = {
   ExampleClient: "./Example.client.tsx",
+  StoryMapJS: "./StoryMapJS.client.tsx",
 };
